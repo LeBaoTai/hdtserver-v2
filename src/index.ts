acb0df1a74c07611f6228837e42520f6edff1ebb
lebaotai <fnight644@gmail.com>
2024-12-05 15:16:19 +0700

    remove serverless

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express, { json, urlencoded } from 'express'
 import categoriesRoutes from './routes/categories/index.js'
 import ordersRoutes from './routes/orders/index.js'
 import productRoutes from './routes/products/index.js'
-import serverless from 'serverless-http'
 
 const port = 3000
 
@@ -18,10 +17,6 @@ app.use('/products', productRoutes)
 app.use('/orders', ordersRoutes)
 app.use('/categories', categoriesRoutes)
 
-if (process.env.NODE_ENV === 'dev') {
-  app.listen(port, () => {
-    console.log(`Server have stated with port ${port}`)
-  })
-}
-
-export const handler = serverless(app);
+app.listen(port, () => {
+  console.log(`Server have stated with port ${port}`)
+})
